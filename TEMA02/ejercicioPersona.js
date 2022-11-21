39f489b98987f2c0387fcc857072a92305f1468f
NicoManfredi <77796768+NicoManfredi@users.noreply.github.com>
2022-11-21 18:12:43 -0300

    Add files via upload

---
+++ TEMA02/ejercicioPersona.js
@@ -0,0 +1,16 @@
+// Ejercicio Tema 2 - Curso JavaScript Básico, OpenBootcamp
+
+const persona = {
+    nombre: "Nicolas Manfredi",
+    edad: 25,
+    desarrollador: true,
+    nacimiento: new Date(1996, 11, 09),
+    libroFavorito: {
+        titulo: "El Hobbit",
+        autor: "J.R.R.Tolkien",
+        fecha: new Date(1937, 09, 21),
+        url: "https://www.amazon.com/-/es/J-R-Tolkien-ebook/dp/B0064T074K",
+    }
+}
+
+console.log(persona);
